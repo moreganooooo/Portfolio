c8addcdc832795e4000a86fa7f7fb5c943281011
moreganooooo <escott.morgan@gmail.com>
2025-06-12 20:32:08 -0400

    Commit

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+	readonly PUBLIC_SITE_URL: string
+	readonly PUBLIC_GOOGLE_ANALYTICS_ID?: string
+	// Add more as needed
+}
+
+interface ImportMeta {
+	readonly env: ImportMetaEnv
+}
